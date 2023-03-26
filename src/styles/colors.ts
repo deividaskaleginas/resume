eb642a50a34e590ab811297112608387faca969b
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-03-26 08:20:07 +0300

    feature: created project

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  background: "#1E1E1E",
+  white: "#FFFFFF",
+  grey1: "#C0C0C0",
+  grey2: "#C0C0C0",
+  purple: "#865BFF",
+  red: "#FB4E78",
+  pink: "C93B90",
+  blue: "#0056D2",
+  green: "#09A72C",
+  darkBlue: "#3422FF",
+};
